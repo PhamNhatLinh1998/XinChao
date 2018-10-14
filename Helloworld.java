782dcc95143b157eddc62126240cc9d603db1d26
PhamNhatLinh <51600045@student.tdtu.edu.vn>
2018-10-14 09:48:58 +0700

    master branch revision 2

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Helloworld {
 	public static void main(String[] args){
-			   System.out.print("Hello World");
+			   System.out.print("Hello World I am Linh");
 	}
 }
