00a019e367d21b9b841a49b76c91121aeec22db3
PhamNhatLinh <51600045@student.tdtu.edu.vn>
2018-10-14 21:57:51 +0700

    master branch revision 5

--- Helloworld.java
+++ Helloworld.java
@@ -1,11 +1,33 @@
 import java.util.*;
-public class Helloworld {
+import java.io.*;
+public class Helloworld{
 	public static void main(String[] args){
-		String s="";
-		System.out.print("Enter Name: ");
-		Scanner sc=new Scanner(System.in);
-		s=sc.nextLine();
-		       System.out.print("Hello World This is TDTU"+"\n");
-			   System.out.print("Hello World I am"+" "+s);
-	}
+		String filename;
+		ArrayList<String> name=new ArrayList<String>();
+		String s=" ";
+		String s1=" ";
+		int count=0;
+            System.out.print("Please input filename: ");
+            Scanner scanner = new Scanner(System.in); 
+            filename = scanner.nextLine();
+	        FileReader file = null;
+	        try{
+			file = new FileReader(filename);
+			Scanner sc= new Scanner(file);
+			while(sc.hasNextLine()){
+			s=sc.nextLine();
+			s1=s.trim();
+			    name.add(s1);
+				count ++;
+			}
+			   System.out.print("Hello World This is TDTU"+"\n");
+			   System.out.print("Total"+" "+count+" "+"Students"+"\n");
+			   for(int i = 0;i<name.size();i++){
+			   System.out.print("Hello World I am"+" "+name.get(i)+"\n");
+	            }
+			}
+			catch(Exception e) {
+			e.printStackTrace();
+		}
+	}				       
 }
