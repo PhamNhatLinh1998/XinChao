cd0e0d4be8dc2e64a90f8554d7e4e9f68f00a6ea
PhamNhatLinh <51600045@student.tdtu.edu.vn>
2018-10-14 09:58:39 +0700

    tdtu branch revision 1

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Helloworld {
 	public static void main(String[] args){
-			   System.out.print("Hello World I am Linh");
+			   System.out.print("Hello World I am TDTU");
 	}
 }
